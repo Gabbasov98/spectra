4e8050c4f14ee5d918bb93b9496a4271faa4c30d
Marsel Gabbasov <marsel13_98@mail.ru>
2021-10-30 19:27:37 +0300

    1

--- js/index.js
+++ js/index.js
@@ -8,15 +8,19 @@ function teamSlider() {
             nextEl: '.team .swiper-button-next',
             prevEl: '.team .swiper-button-prev',
         },
+        pagination: {
+            el: '.team .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
         breakpoints: {
             320: {
                 slidesPerView: 1,
                 spaceBetween: 0,
 
             },
-            480: {
-                slidesPerView: 2,
-                spaceBetween: 0
+            992: {
+                slidesPerView: 1,
             },
             1200: {
                 slidesPerView: 2,
@@ -26,9 +30,40 @@ function teamSlider() {
     });
 }
 
+function whySlider() {
+    var swiper = new Swiper('.why .swiper-container', {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        speed: 1000,
+        loop: true,
+        pagination: {
+            el: '.why .swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 30,
+
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 2,
+                spaceBetween: 10
+            }
+        }
+    });
+}
+
 
 $(document).ready(function() {
     teamSlider()
+    if ($(window).innerWidth() < 992) {
+        whySlider()
+    }
 
 
     $(".offer__tab").click(function() {
@@ -49,4 +84,22 @@ $(document).ready(function() {
 
     })
 
+    $(".header__burger").click(function() {
+        $(this).toggleClass("header__burger--active")
+        $(".header__right").slideToggle()
+    })
+
+    $(".nav-dropdown-show").click(function() {
+        $(this).addClass("nav-dropdown-show--active")
+    })
+
+    $(document).mouseup(function(e) { // событие клика по веб-документу
+        var div = $(".nav-dropdown-hidden"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            &&
+            div.has(e.target).length === 0 &&
+            $(".nav-dropdown-show").hasClass("nav-dropdown-show--active")) { // и не по его дочерним элементам
+            $(".nav-dropdown-show").removeClass("nav-dropdown-show--active")
+        }
+    });
 })
